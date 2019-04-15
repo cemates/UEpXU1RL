176e65b6380b943b33e5fe875344bc7359625119
cem <cem@172.23.67.114.wi-fi.pjwstk.edu.pl>
2019-04-15 16:04:46 +0200

    Initial Commit

---
+++ Mobile Programming/ProOne/app/src/main/java/me/cemates/proone/BossAct.java
@@ -0,0 +1,62 @@
+package me.cemates.proone;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import me.cemates.proone.R;
+
+public class BossAct extends AppCompatActivity {
+
+    TextView textScreen, textQuestion, textBtn;
+    ImageView bigboss;
+    Animation smalltobig;
+    Button textTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boss);
+
+        smalltobig = AnimationUtils.loadAnimation(this, R.anim.smalltobig);
+
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/FredokaOneRegular.ttf");
+
+        textQuestion = (TextView) findViewById(R.id.textQuestion);
+        textScreen = (TextView) findViewById(R.id.textScreen);
+        textTitle = (Button) findViewById(R.id.textTitle);
+        textBtn = (TextView) findViewById(R.id.textBtn);
+
+        bigboss = (ImageView) findViewById(R.id.bigboss);
+        bigboss.startAnimation(smalltobig);
+
+
+        textQuestion.setTypeface(typeface);
+        textScreen.setTypeface(typeface);
+        textTitle.setTypeface(typeface);
+        textBtn.setTypeface(typeface);
+
+        textTitle.setOnClickListener(new OnClickListener() {
+            @Override
+
+                public void onClick (View arg0){
+                    Intent myIntent = new Intent(BossAct.this,
+                            MainActivity.class);
+                    startActivity(myIntent);
+                }
+
+
+
+        });
+    }
+
+}
---
+++ Mobile Programming/ProOne/app/src/main/java/me/cemates/proone/SecondLevel.java
@@ -0,0 +1,18 @@
+package me.cemates.proone;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SecondLevel extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+
+
+
+}
